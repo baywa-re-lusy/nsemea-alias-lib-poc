cbb843c4a16d8ab7377b6ea43e32790acef46e01
dariodds <dario.dasilva.private@gmail.com>
2025-03-31 13:57:15 +0200

    include entry point

---
+++ src/bin/index.ts
@@ -0,0 +1,8 @@
+import { program } from 'commander';
+import { replaceAlias } from '..';
+
+const { version } = require('../../package.json');
+
+program
+  .name('nsemea-alias')
+  .version(version);
