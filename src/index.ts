413a73f91623d90c41131c164a52af45f8740a44
dariodds <dario.dasilva.private@gmail.com>
2025-03-31 15:07:15 +0200

    reading tsconfig.json file content

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { resolve } from 'path';
+import { Json } from 'mylas';
 
 export async function replaceAlias() {
   console.log('Running replace alias');
   console.log('Resolve tsconfig.json path');
   const configFilePath = resolve(process.cwd(), 'tsconfig.json');
   console.log('Path tsconfig.json :', configFilePath);
+  const configFileData = Json.loadS(configFilePath, true);
+  console.log('TSconfig.json content:', configFileData);
 }
