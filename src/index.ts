cb8b124f0d4abd9bd275e271d989842cf52cf5c2
dariodds <dario.dasilva.private@gmail.com>
2025-03-31 14:57:33 +0200

    resolve tsconfig.json file path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { resolve } from 'path';
+
 export async function replaceAlias() {
   console.log('Running replace alias');
+  console.log('Resolve tsconfig.json path');
+  const configFilePath = resolve(process.cwd(), 'tsconfig.json');
+  console.log('Path tsconfig.json :', configFilePath);
 }
