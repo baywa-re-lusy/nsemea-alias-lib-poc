5ecd77f24f78d780ac28902f1cdf3af14a141344
dariodds <dario.dasilva.private@gmail.com>
2025-03-31 15:38:42 +0200

    reading tsconfig.json file content

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import { resolve } from 'path';
+import { join, isAbsolute, resolve } from 'path';
 import { Json } from 'mylas';
 
 export async function replaceAlias() {
   console.log('Running replace alias');
+
+  const config = {
+    baseUrl: '',
+    outDir: ''
+  };
+
   console.log('Resolve tsconfig.json path');
+
+  // Get projects tsconfig file.
   const configFilePath = resolve(process.cwd(), 'tsconfig.json');
   console.log('Path tsconfig.json :', configFilePath);
-  const configFileData = Json.loadS(configFilePath, true);
-  console.log('TSconfig.json content:', configFileData);
+
+  // Get projects base url and output dir from tsconfig file.
+  const { compilerOptions: { baseUrl, outDir } = {
+    baseUrl: undefined,
+    outDir: undefined }
+  } = Json.loadS(configFilePath, true);
+  console.log('TSconfig.json content baseUrl:', baseUrl);
+  console.log('TSconfig.json content outDir:', outDir);
+
+  if (baseUrl) {
+    config.outDir = baseUrl;
+  }
+  if (outDir) {
+    config.outDir = isAbsolute(outDir) ? outDir : join(baseUrl, outDir);
+  }
+
+  console.log('Config :', config)
+
 }
