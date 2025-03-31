51f52b5184e32015b29246f7708360a80ab73e9d
dariodds <dario.dasilva.private@gmail.com>
2025-03-31 15:52:30 +0200

    getting .js files from tsc output dir

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,51 @@
 import { join, isAbsolute, resolve } from 'path';
 import { Json } from 'mylas';
+import { globbySync } from 'globby';
 
 export async function replaceAlias() {
   console.log('Running replace alias');
+  const config = loadConfigFile();
 
-  const config = {
-    baseUrl: '',
-    outDir: ''
-  };
+  // Finding files and changing alias paths
+  const posixOutput = config.outDir.replace(/\\/g, '/').replace(/\/+$/g, '');
+  const globPattern = [
+    `${posixOutput}/**/*.js`,
+    `!${posixOutput}/**/node_modules`
+  ];
+  console.log('Search pattern:', globPattern);
+  const files = globbySync(globPattern, {
+    dot: true,
+    onlyFiles: true
+  });
+  console.log('Found files:', files);
 
-  console.log('Resolve tsconfig.json path');
+}
+
+function loadConfigFile() {
+  const config = { baseUrl: '', outDir: '' };
+
+  console.log('loadConfigFile > Resolve tsconfig.json path');
 
   // Get projects tsconfig file.
   const configFilePath = resolve(process.cwd(), 'tsconfig.json');
-  console.log('Path tsconfig.json :', configFilePath);
+  console.log('loadConfigFile > Path tsconfig.json :', configFilePath);
 
   // Get projects base url and output dir from tsconfig file.
   const { compilerOptions: { baseUrl, outDir } = {
     baseUrl: undefined,
     outDir: undefined }
   } = Json.loadS(configFilePath, true);
-  console.log('TSconfig.json content baseUrl:', baseUrl);
-  console.log('TSconfig.json content outDir:', outDir);
+  console.log('loadConfigFile > TSconfig.json content baseUrl:', baseUrl);
+  console.log('loadConfigFile > TSconfig.json content outDir:', outDir);
 
   if (baseUrl) {
     config.baseUrl = baseUrl;
   }
   if (outDir) {
-    console.log('isAbsolute(outDir) :', isAbsolute(outDir));
     config.outDir = isAbsolute(outDir) ? outDir : join(baseUrl, outDir);
   }
 
-  console.log('Config :', config)
+  console.log('loadConfigFile > Config :', config)
 
+  return config;
 }
